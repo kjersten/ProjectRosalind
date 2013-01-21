b5405f680b7353e9f1daa0d24223755253e03b99
Kjersten Elias <kjersten.elias@gmail.com>
2013-01-20 21:24:21 -0800

    cleaning up this code a little bit... the time logging aspects of it could still be factored out into a wrapper function

--- src/main/scala/net/bitzoo/rosalind/L1_DNA_CountingNucleotides.scala
+++ src/main/scala/net/bitzoo/rosalind/L1_DNA_CountingNucleotides.scala
@@ -3,19 +3,34 @@ package net.bitzoo.rosalind
 object CountingNucleotides {
 
   def getAnswer(polymer: String): String = {
+    
+    // how long does it take to execute doTheFirstThingYouThinkOf?
+    val start1 = System.currentTimeMillis()
+    val result1 = doTheFirstThingYouThinkOf(polymer)
+    val end1 = System.currentTimeMillis();
+    Console.println("Iterating through the dataset once for each base took " + (end1 - start1) + " milliseconds\n\n")
+
+    // how long does it take to execute useMapReduce?
+    val start2 = System.currentTimeMillis()
+    val result2 = useMapReduce(polymer)
+    val end2 = System.currentTimeMillis()
+    Console.println("Using mapreduce took " + (end2 - start2) + " milliseconds\n\n")
+
+    return result2
+  }
+
+  def doTheFirstThingYouThinkOf(polymer: String): String = {
     val bases = Set('A', 'C', 'G', 'T')
     var result = Map[Char, Int]()
-
-    val start1 = System.currentTimeMillis()
     for (base <- bases) {
       result = result + (base -> polymer.count(_ == base))
     }
-    val end1 = System.currentTimeMillis();
-    Console.println(result.mkString(", "))
-    Console.println("Iterating through the dataset once for each base took " + (end1 - start1) + " milliseconds\n\n")
+    
+    result.mkString(", ")
+  }
 
-    val start2 = System.currentTimeMillis()
-    val result2: Tuple4[Int, Int, Int, Int] = polymer.
+  def useMapReduce(polymer: String): String = {
+    val result: Tuple4[Int, Int, Int, Int] = polymer.
       map(nt =>
         if (nt == 'A') (1, 0, 0, 0)
         else if (nt == 'C') (0, 1, 0, 0)
@@ -24,12 +39,7 @@ object CountingNucleotides {
         else /* bad data */ (0, 0, 0, 0)).
       reduce((x, y) =>
         (x._1 + y._1, x._2 + y._2, x._3 + y._3, x._4 + y._4))
-    val end2 = System.currentTimeMillis();
-
-    println(result2)
-    Console.println("Using mapreduce took " + (end2 - start2) + " milliseconds\n\n")
-    
-    result2.toString()
-
+        
+    result.toString()
   }
 }
