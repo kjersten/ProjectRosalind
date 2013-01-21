2e9b6feebf2a6cc14973ee78d37e59aca45e249f
Kjersten Elias <kjersten.elias@gmail.com>
2013-01-20 19:05:57 -0800

    automating the tests I ran when I wrote the code.

--- src/main/scala/net/bitzoo/rosalind/L1_DNA_CountingNucleotides.scala
+++ src/main/scala/net/bitzoo/rosalind/L1_DNA_CountingNucleotides.scala
@@ -1,34 +1,35 @@
 package net.bitzoo.rosalind
 
-object CountingNucleotides extends App {
+object CountingNucleotides {
 
-  private val polymer: String = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
-//  private val polymer = "AACCGCGGGGGCCAGAGGTACCTTACCATATCGCCGTCCATCTCAATCAATGGAATGTTAGGATAGATGCTGTTCTCGTAATTGGCTTTACCCGAGCCGAAATACGCTGCCTGAAACTGGTGCTGAGCATGGCTCGGAAGTCCGGTGAGATTATCGGACATGATTCCCTTTACCTTACCTGAACTTGAAACTAAATCGGATGGCGATCATCGGAATACTAGAGATCCGCTCAGCGCGGTACCAGGTGCCTCTTGGTACCCTGACTGGATGGTCCAGAATCAATGCCTGCTCATCGGGATAAAAGCAGGGATATATGCCACTAACTCTGGTCTCGCGTTTCACCGTCGACGATGTTCGACCAGGGCGACATGCTTGGCCTTCTTAGTCGCAGGCAGACAGTATGTGGCGTCATCGAGAGGCGGCTCCCGACGAATTTATGTTATAATTTAAAATGCCGGAACGTAATTAATAAATCGGGCCACTCTACGTGGAACTTAATCATCACTCATGAATTAGCTGCATCTTGGCCTATAGCTACACATAGGCGTCTTATGGGGGGTGGCACCACTATGGTTAAGAATTATTGAAGCGCCTGGTAATGGCCACAGATACCAGACCGTTGGGGGCTCTACGATGCTTCTCAATTGTGCCCCAGAGATTGTCCAACGCGAAATAAGTATCAGCTATGGGGGCTGAGGGACTGCAGAAGCACTACTCACCAAGCCGGGACTCCCCAATCTTCAGGCGGCTCTTCCGAAACTTTGCGAGTTTCATCTTGCTTCGTATCCTTCGAGTTACTTTATTTTATGCCGACGTGTTACTTGACCCAG"
-  private val bases = Set('A', 'C', 'G', 'T')
-  private var result = Map[Char, Int]()
+  def getAnswer(polymer: String): String = {
+    val bases = Set('A', 'C', 'G', 'T')
+    var result = Map[Char, Int]()
 
-  private val start1 = System.currentTimeMillis()
-  for (base <- bases) {
-    result = result + (base -> polymer.count(_ == base))
+    val start1 = System.currentTimeMillis()
+    for (base <- bases) {
+      result = result + (base -> polymer.count(_ == base))
+    }
+    val end1 = System.currentTimeMillis();
+    Console.println(result.mkString(", "))
+    Console.println("Iterating through the dataset once for each base took " + (end1 - start1) + " milliseconds\n\n")
+
+    val start2 = System.currentTimeMillis()
+    val result2: Tuple4[Int, Int, Int, Int] = polymer.
+      map(nt =>
+        if (nt == 'A') (1, 0, 0, 0)
+        else if (nt == 'C') (0, 1, 0, 0)
+        else if (nt == 'G') (0, 0, 1, 0)
+        else if (nt == 'T') (0, 0, 0, 1)
+        else /* bad data */ (0, 0, 0, 0)).
+      reduce((x, y) =>
+        (x._1 + y._1, x._2 + y._2, x._3 + y._3, x._4 + y._4))
+    val end2 = System.currentTimeMillis();
+
+    println(result2)
+    Console.println("Using mapreduce took " + (end2 - start2) + " milliseconds\n\n")
+    
+    result2.toString()
+
   }
-  private val end1 = System.currentTimeMillis();
-  Console.println(result.mkString(", "))
-  Console.println("Iterating through the dataset once for each base took " + (end1 - start1) + " milliseconds\n\n")
-
-    private val start2 = System.currentTimeMillis()
-  private val result2: Tuple4[Int, Int, Int, Int] = polymer.
-    map(nt =>
-           if (nt == 'A') (1, 0, 0, 0)
-      else if (nt == 'C') (0, 1, 0, 0)
-      else if (nt == 'G') (0, 0, 1, 0)
-      else if (nt == 'T') (0, 0, 0, 1)
-      else /* bad data */ (0, 0, 0, 0)).
-    reduce((x, y) =>
-      (x._1 + y._1, x._2 + y._2, x._3 + y._3, x._4 + y._4))
-  private val end2 = System.currentTimeMillis();
-  
-  println(result2)
-  Console.println("Using mapreduce took " + (end2 - start2) + " milliseconds\n\n")
-
-
 }
